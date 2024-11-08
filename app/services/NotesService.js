21582524c54c7b306bb25471a0ca352dfcde8332
Michael Giesler <michael.giesler1@icloud.com>
2024-11-08 13:21:37 -0700

    class NotesService

--- app/services/NotesService.js
+++ app/services/NotesService.js
@@ -1,2 +1,17 @@
 import { appState } from "../AppState.js";
 import { Note } from "../models/Note.js";
+
+class NotesService {
+  createNote(formData) {
+    const newNote = new Note({
+      title: formData.title,
+      color: formData.color,
+      body: formData.body
+    });
+    appState.notes.push(newNote);
+    appState.saveNotes();
+    appState.setActive(newNote);
+  }
+
+
+}
