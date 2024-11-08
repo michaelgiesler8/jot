f3c78f9ab86cdf25304b3ecbfcfc31c102eb4e71
Michael Giesler <michael.giesler1@icloud.com>
2024-11-08 12:53:05 -0700

    function _drawNotesList

--- app/controllers/NotesController.js
+++ app/controllers/NotesController.js
@@ -1,2 +1,11 @@
 import { notesService } from "../services/NotesService.js";
 import { appState } from "../AppState.js";
+
+
+function _drawNotesList() {
+  const notes = appState.notes;
+  let template = notes.map(note => note.ListTemplate).join("");
+  document.getElementById("notesList").innerHTML = template;
+  document.getElementById("noteCount").innerHTML = notes.length;
+}
+
