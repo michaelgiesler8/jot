d25313716778c6f498f0284e9bf7cf91b50bae99
Michael Giesler <michael.giesler1@icloud.com>
2024-11-08 13:12:44 -0700

    model export class

--- app/models/Note.js
+++ app/models/Note.js
@@ -1,2 +1,12 @@
 import { generateId } from "../utils/GenerateId.js";
 
+export class Note {
+  constructor({ id = generateId(), title, color = "#ffffff", body = "", createdAt = new Date(), updatedAt = new Date() }) {
+    this.id = id;
+    this.title = title;
+    this.color = color;
+    this.body = body;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+}
