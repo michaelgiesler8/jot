b675a171b4831117a5f7a2448b85c16e244d365b
Michael Giesler <michael.giesler1@icloud.com>
2024-11-11 15:15:50 -0700

    noteservice.js timestamp update

--- app/services/NotesService.js
+++ app/services/NotesService.js
@@ -8,15 +8,15 @@ class NotesService {
       color: formData.color,
       body: formData.body
     });
-    appState.addNote(newNote); // Use the new method to add and save the note
-    appState.setActive(newNote); // Set the new note as the active note
+    appState.addNote(newNote);
+    appState.setActive(newNote);
   }
 
   setActiveNote(id) {
     const note = appState.notes.find(note => note.id === id);
     if (note) {
-      // appState.setActive(note);
-      appState.activeNote = note
+
+      appState.setActive(note)
       appState.emit("activeNote")
     }
   }
@@ -25,15 +25,15 @@ class NotesService {
     const note = appState.activeNote;
     if (note) {
       note.body = body;
-      note.updatedAt = new Date();
-      appState.saveNotes(); // Save changes to local storage
+      note.updatedAt = new Date():
+      appState.saveNotes();
     }
   }
 
   deleteNoteById(id) {
     if (confirm("Are you sure you want to delete this note?")) {
-      appState.deleteNoteById(id); // Use the new method to delete and save the updated notes
-      appState.setActive(null); // Clear the active note if it was deleted
+      appState.deleteNoteById(id);
+      appState.setActive(null);
     }
   }
 }
